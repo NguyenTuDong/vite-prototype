228b10aed354c090b8e8c8b63f9106cc4557ea98
NguyenTuDong <tudong.ng@gmail.com>
2025-04-19 10:50:30 +0700

    feat: add devtools to examples

--- examples/nunjucks/vite.config.ts
+++ examples/nunjucks/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import prototype from "vite-plugin-prototype";
 import nunjucks from "vite-plugin-prototype-nunjucks";
+import devtools from "vite-plugin-prototype-devtools";
 
 export default defineConfig({
   root: "./src",
-  plugins: [prototype(), nunjucks()],
+  plugins: [prototype(), nunjucks(), devtools()],
 });
